4b27dfa6b5402cd1da62dda0d3cd533fd5518622
peterccc114 <prettyboy114@mail.com>
2017-07-27 11:51:51 -0700

    chore:generate share bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PagelayoutComponent } from './pagelayout/pagelayout.component';
+import { HeaderBarComponent } from './pagelayout/src/app/pagelayout/header-bar/header-bar.component';
+import { SideBarComponent } from './pagelayout/src/app/pagelayout/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagelayoutComponent,
+    HeaderBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
